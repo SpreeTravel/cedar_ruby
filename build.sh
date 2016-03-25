d4840929aab6389a11eda7433e9ee23cbd015ff7
D.H. Bahr <dhbahr@gmail.com>
2016-03-25 09:10:00 -0400

    Not running 'exec' on build

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ set -x
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-exec $SHELL
+. ~/.bashrc
 
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
-exec $SHELL
+. ~/.bashrc
 
 git clone https://github.com/rbenv/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
 
